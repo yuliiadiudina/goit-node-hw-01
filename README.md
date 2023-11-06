c97bd190c5ee364ab8cbb165cf01e39a9765c25e
Yuliia Diudina <yuliiadiudina@gmail.com>
2023-11-06 23:14:48 +0200

    cli

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # goit-node-hw-01
+
+list: https://monosnap.com/file/qC5LbqJL9gx1gaKo70qyYiBYWRfF4K
+get: https://monosnap.com/file/uIVX2CqD4ia2AdEEkubvyZEwSMqF0t
+add: https://monosnap.com/file/W3OQt3NVTkzjP2zhZX4y1czuzAs6iu
+remove: https://monosnap.com/file/fd9fC6yrHe1y73LuJAYSzTRs8PsMRH
---
+++ index.js
@@ -0,0 +1,48 @@
+const {
+    listContacts,
+    getContactById,
+    removeContact,
+    addContact,
+} = require ("./contacts");
+ const contacts = require ("./contacts")
+const { Command } = require ('commander');
+const program = new Command();
+
+program
+  .option('-a, --action <type>', 'choose action')
+  .option('-i, --id <type>', 'user id')
+  .option('-n, --name <type>', 'user name')
+  .option('-e, --email <type>', 'user email')
+  .option('-p, --phone <type>', 'user phone');
+
+program.parse(process.argv);
+
+const argv = program.opts();
+
+async function invokeAction({ action, id, name, email, phone }) {
+  switch (action) {
+    case 'list':
+      const all = await contacts.listContacts();
+      console.table(all);
+      break;
+
+    case 'get':
+      const oneContact = await contacts.getContactById(id);
+      console.log(oneContact);
+      break;
+
+    case 'add':
+      const newContact =  await contacts.addContact(name, email, phone);
+      console.log(newContact);
+
+    case 'remove':
+      const removedContact = await contacts.removeContact(id);
+      console.log(removedContact);
+      break;
+
+    default:
+      console.warn('\x1B[31m Unknown action type!');
+  }
+}
+
+invokeAction(argv);
